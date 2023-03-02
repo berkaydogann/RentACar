87d5c9240a574d582ce09e984d57c430b3a8fe69
Berkay Aydoğan <berkaydogann98@gmail.com>
2023-03-02 20:59:47 +0300

    Brand Model added.

---
+++ src/main/java/com/springExample/rentACar/entities/concretes/Model.java
@@ -0,0 +1,25 @@
+package com.springExample.rentACar.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name = "models")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+}
